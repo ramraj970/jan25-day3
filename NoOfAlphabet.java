1c045930301632ca24bf29a1bc85f630bbb41dc2
ramraj970 <chandikavenkatbabu@gmail.com>
2025-01-25 15:51:55 +0530

    loc

---
+++ NoOfAlphabet.java
@@ -0,0 +1,19 @@
+
+public class NoOfAlphabet {
+
+	public static void main(String[] args) {
+		for(int i=630,k=0,m=0;i>=500;i++) {
+			int num = i;
+			int counter =0;
+			for(int p=1;p<=num;p++) {
+				if(num % p == 0) counter++;
+			}
+			if(counter == 2) {
+				System.out.println("Last prime number...."+num);
+				break;
+			} //if counter
+			
+		} // for i
+	}
+
+}
